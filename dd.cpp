c49e3fe639d8e0efc4c28ff034185af695cb603d
msarezaghfsfgdfd <148532638+mohammadrezadm@users.noreply.github.com>
2023-10-20 17:39:02 +0700

    Add files via upload

---
+++ dd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+int c;
+b=23;
+a=12;
+c =4;
+cout<<"ellodj\n"<<a*3<<endl;
+printf("is in dor:%d\n",b);
+printf_s("is a dojhf:%d\n",c);
+ cout<<a,b,c;
+if (30<40<45<60) {
+cout<<"30 dsai40 jhghgf 45 jhsfgh60";
+}
+return 0;
+}

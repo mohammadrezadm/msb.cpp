179009152a5945b05a6c1cab9b5c6500d10ad123
msarezaghfsfgdfd <148532638+mohammadrezadm@users.noreply.github.com>
2023-10-20 16:33:32 +0700

    Add files via upload

---
+++ m.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"entr your name:"<<a*2 <<endl;
+    cout<<"your a jdfh:" <<a-2 <<endl;
+    cout<<"entr safsfafsa:"<< a+2  <<endl;
+    cout<<"entr safsfafsa:" <<a/2   <<endl;
+    cin>> a;
+
+   
+}
